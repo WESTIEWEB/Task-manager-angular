3f2e68abaa39d97ad9809d80a701824df81a0397
CHIBUIKE ILO <82292997+WESTIEWEB@users.noreply.github.com>
2025-01-16 01:59:43 +0100

    add matdialog module to open task form on add click

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatListModule } from '@angular/material/list';
 import { TaskModel } from '../models/task.model';
 import { TaskService } from '../services/task.service';
 import { FormsModule } from '@angular/forms';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @Component({
   selector: 'app-task-list',
@@ -16,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatSelectModule,
     MatListModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule
   ],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
@@ -27,12 +30,33 @@ export class TaskListComponent implements OnInit {
   priorityFilter = '';
   statusFilter = '';
 
-  constructor(private taskService: TaskService) {}
+  constructor(
+    private taskService: TaskService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.loadTasks();
   }
 
+  openTaskForm(): void {
+    const dialogRef = this.dialog.open(TaskFormComponent, {
+      width: '600px',
+      panelClass: 'task-dialog'
+    });
+  
+    // After the dialog is closed, handle the returned task data
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // Add the new task to the tasks array
+        this.tasks.push(result);
+  
+        // Reapply the filters to ensure the new task is shown based on the current filters
+        this.applyFilters();
+      }
+    });
+  }
+  
   loadTasks(): void {
     this.tasks = this.taskService.getTasks();
     this.applyFilters();
